7da3835fb49b3718d70a9147221172936ae8ae13
unnine <eonninej@gmail.com>
2025-04-04 01:28:34 +0900

    qwd

--- FlowBoard.js
+++ FlowBoard.js
@@ -46,27 +46,160 @@
 
       function DragBoard($el, doublyLinkedMap) {
         this.store = doublyLinkedMap;
-        this.$board = $el;
-        this.dragItems = [];
+        this.$self = $el;
+        this.drag = {
+          el: null,
+          items: [],
+          active: false,
+          activeItem: null,
+          eventListeners: null,
+        };
+        this.hooks = {
+          [this.EVENT.DRAG_START]: null,
+          [this.EVENT.DRAGGING]: null,
+          [this.EVENT.DRAG_END]: null,
+        };
+        this.init();
+        return this.returnObject();
       }
 
       (function DragBoardPrototype() {
 
+        this.EVENT = {
+          DRAG_START: 'dragStart',
+          DRAG_END: 'dragEnd',
+          DRAGGING: 'dragging',
+        };
+
+        this.init = function() {
+          this.bindEventListeners();
+        }
+
+        this.destroy = function() {
+          this.releaseEventListeners();
+          this.removeElements();
+          
+          this.store = null;
+          this.$self = null;
+          this.drag = null;
+          this.hooks = null;
+        }
+
+        this.bindEventListeners = function() {
+          this.drag.eventListeners = {
+            dragStart: this.dragStart.bind(this),
+            dragging: this.dragging.bind(this),
+            dragEnd: this.dragEnd.bind(this),
+          };
+
+          this.$self.addEventListener("touchstart", this.drag.eventListeners.dragStart);
+          this.$self.addEventListener("touchmove", this.drag.eventListeners.dragging);
+          this.$self.addEventListener("touchend", this.drag.eventListeners.dragEnd);
+          this.$self.addEventListener('mousedown', this.drag.eventListeners.dragStart);
+          this.$self.addEventListener('mousemove', this.drag.eventListeners.dragging);
+          this.$self.addEventListener('mouseup', this.drag.eventListeners.dragEnd);
+        }
+        
+        this.releaseEventListeners = function() {
+          this.$self.removeEventListener("touchstart", this.drag.eventListeners.dragStart);
+          this.$self.removeEventListener("touchmove", this.drag.eventListeners.dragging);
+          this.$self.removeEventListener("touchend", this.drag.eventListeners.dragEnd);
+          this.$self.removeEventListener('mousedown', this.drag.eventListeners.dragStart);
+          this.$self.removeEventListener('mousemove', this.drag.eventListeners.dragging);
+          this.$self.removeEventListener('mouseup', this.drag.eventListeners.dragEnd);
+
+          this.drag.eventListeners = null;
+        }
+
+        this.dragStart = function(e) {
+          const dragItem = this.getSelectedDragItem(e.target);
+  
+          if (dragItem != null) {
+            this.drag.active = true;
+            this.drag.activeItem = dragItem;
+            this.drag.activeItem.moveStart(this.clientX(e), this.clientY(e));
+          }
+          this.publishHook(this.EVENT.DRAG_START, e);
+        }
+
+        this.dragging = function(e) {
+          if (!this.active) {
+            return;
+          }
+          if (this.activeItem == null) {
+            return;
+          }
+          e.preventDefault();
+          this.activeItem.move(this.clientX(e), this.clientY(e));
+          this.publishHook(this.EVENT.DRAGGING, e);
+        }
+  
+        this.dragEnd = function(e) {
+          this.active = false;
+  
+          if (this.activeItem != null) {
+            this.activeItem.moveEnd();
+            this.activeItem = null;
+          }
+          this.publishHook(this.EVENT.DRAG_END, e);
+        }
+
+        this.publishHook = function(eventType, originalEvent) {
+          originalEvent.preventDefault();
+          if (this.hooks[eventType] == null) {
+            return;
+          }
+          this.hooks[eventType]({ eventType, originalEvent });
+        }
+
+        this.registerHook = function (eventType, listener) {
+          this.hooks[eventType] = listener;
+        }
+
+        this.getSelectedDragItem = function($selectedItem) {
+          for (let item of this.drag.items) {
+            if (item.equals($selectedItem)) {
+              return item;
+            }
+          }
+          return null;
+        }
+
+        this.isTouched = function(e) {
+          return e.type.startsWith('touch');
+        }
+
+        this.clientX = function(e) {
+          if (this.isTouched(e)) {
+            return e.touches[0].clientX;
+          }
+          return e.clientX;
+        }.bind(this);
+  
+        this.clientY = function(e) {
+          if (this.isTouched(e)) {
+            return e.touches[0].clientY;
+          }
+          return e.clientY;
+        }.bind(this);
+  
         this.draggable = function(selector) {
-          const $els = this.$board.querySelectorAll(selector);
+          const $els = this.$self.querySelectorAll(selector);
           if ($els.length <= 0) {
             return;
           }
           for (let $el of $els) {
-            this.dragItems.push(new Drag($el));
+            this.drag.items.push(new Drag($el));
           }
-          console.log(this.dragItems);
-          return this.returnObject;
         }
 
         this.returnObject = function() {
           return {
             draggable: this.draggable.bind(this),
+            destroy: this.destroy.bind(this),
+            onDragStart: this.registerHook.bind(this, this.EVENT.DRAG_START),
+            onDragging: this.registerHook.bind(this, this.EVENT.DRAGGING),
+            onDragEnd: this.registerHook.bind(this, this.EVENT.DRAG_END),
           };
         }
 
@@ -81,10 +214,130 @@
 
       (function DragFactory() {
         'use strict'
+
+        const defaultOption = {
+          svg: true,
+        };
   
-        function Drag($el) {
-  
+        function Drag($el, customOption) {
+          this.items = [ this.makeDragItem($el) ];
+          this.option = {
+            ...defaultOption,
+            ...customOption,
+          };
+          this.coord = {
+            initX: 0,
+            initY: 0,
+            startedX: 0,
+            startedY: 0,
+            currentX: 0,
+            currentY: 0,
+            offsetX: 0,
+            offsetY: 0,
+          };
+          return this.returnObject();
         }
+
+        (function DragPrototype() {
+
+          this.destroy = function() {
+            if (this.items.length > 0) {
+              for (let item of this.items) {
+                item.$el.remove();
+              }
+            }
+            this.items = null;
+            this.option = null;
+            this.coord = null;
+          }
+
+          this.moveStart = function(clientX, clientY) {
+            const { x, y } = this.getCoordinate(clientX, clientY);
+            this.startedX = x - this.offsetX;
+            this.startedY = y - this.offsetY;
+          }.bind(this);
+    
+          this.move = function(clientX, clientY) {
+            const { x, y } = this.getCoordinate(clientX, clientY);
+            this.coord.currentX = x - this.coord.startedX;
+            this.coord.currentY = y - this.coord.startedY;
+    
+            this.coord.offsetX = this.coord.currentX;
+            this.coord.offsetY = this.coord.currentY;
+    
+            for (let item of this.items) {
+              if (this.isSVG(item)) {
+                this.coordinate(item);
+                continue;
+              }
+              this.translate(item);
+            }
+          }.bind(this);
+    
+          this.moveEnd = function() {
+            this.coord.startedX = this.coord.currentX;
+            this.coord.startedY = this.coord.currentY;
+          }.bind(this);
+
+          this.isSVG = function(item) {
+            return item.$el.tagName === 'svg';
+          }
+
+          this.getCoordinate = function(clientX, clientY) {
+            if (this.option.svg) {
+              const CTM = this.items[0].$el.getScreenCTM();
+              return {
+                x: clientX / CTM.a,
+                y: clientY / CTM.d,
+              };
+            }
+            return {
+              x: clientX,
+              y: clientY,
+            };
+          }
+
+          this.coordinate = function(item) {
+            item.$el.setAttribute('x', this.coord.currentX + item.x);
+            item.$el.setAttribute('y', this.coord.currentY + item.y);
+          }
+
+          this.translate = function(item) {
+            item.$el.style.transform = `translate3d(${this.coord.currentX}px, ${this.coord.currentY}px, 0)`;
+          }
+
+          this.equals = function($el) {
+            return this.items[0].$el == $el;
+          }
+
+          this.makeDragItem = function($el) {
+            const { x, y } = $el.getBoundingClientRect();
+            return { $el, x, y };
+          }
+
+          this.returnObject = function() {
+            const _this = this;
+
+            return {
+              moveStart(clientX, clientY) {
+                _this.moveStart(clientX, clientY);
+                return this;
+              },
+              move(clientX, clientY) {
+                _this.move(clientX, clientY);
+                return this;
+              },
+              moveEnd() {
+                _this.moveEnd();
+              },
+              equals($el) {
+                _this.equals($el);
+                return this;
+              },
+            };
+          }
+
+        }).call(Drag.prototype);
         
         return Drag;
       }()),
@@ -100,7 +353,6 @@
         this.tail = null;
         this.map = Object.create(null);
         this.length = 0;
-
         return this.returnObject();
       }
 
