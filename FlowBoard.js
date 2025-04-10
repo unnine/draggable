a98c58503746ac69206b7eeca6fae967d33447a7
unnine <eonninej@gmail.com>
2025-04-10 17:59:16 +0900

    add css

--- FlowBoard.js
+++ FlowBoard.js
@@ -20,12 +20,6 @@
 
     const dragBoards = __DoublyLinkedMapFactory.create();
 
-    const expect = (key) => {
-      return {
-        is: (value) => key === value,
-      };
-    }
-
     function createSVGTag(tagName) {
       return document.createElementNS('http://www.w3.org/2000/svg', tagName);
     }
@@ -118,7 +112,6 @@
           for (let $el of $els) {
             this.dragItems.push(new Drag($el));
           }
-          console.log(this.dragItems);
           return this.returnObject;
         }
 
