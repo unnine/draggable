ce29f7eeeebe52fa0e8314de65b7d6d99d93d9b3
unnine <eonninej@gmail.com>
2025-04-15 09:15:53 +0900

    add Writable

--- module/Writable.js
+++ module/Writable.js
@@ -11,27 +11,28 @@
   else {
     window['DragBoard'] = factory;
   }
-}((function DragBoardFactory(Drag) {
+}((function DragBoardFactory(Drag, __DoublyLinkedMapFactory) {
     'use strict'
 
     function DragBoard($el, option = {}) {     
       this.initialized = false;
       this.option = option;
+      this.mutationObserver = null;
+      // this.store = __DoublyLinkedMapFactory.create();
       this.$board = $el;
+      this.eventListeners = {};
+      this.hooks = {};
+      this.draggable = {
+        selectors: [],
+        $elements: [],
+        items: [],
+      };
       this.state = {
         drag: {
           active: false,
           el: null,
         },
       };
-      this.draggable = {
-        selectors: [],
-        $elements: [],
-        items: [],
-      };
-      this.hooks = {};
-      this.mutationObserver = null;
-      this.eventListeners = {};
       this.returnObject = this.returnObject();
       return this.returnObject;
     }
@@ -267,21 +268,21 @@
       }
 
       this.destroy = function() {
-        this.removeElements();
-        this.releaseEventListeners();
         this.callHook(this.EVENT.LIFE_CYCLE.DESTROY, {
           container: this.$board,
           draggable: this.draggable.$elements,
         });
-        this.returnObject = null;
-        this.eventListeners = null;
+        this.removeElements();
+        this.releaseEventListeners();
         this.mutationObserver = null;
-        this.hooks = null;
+        this.eventListeners = null;
+        this.$board = null;
+        this.returnObject = null;
+        // this.store = null;
         this.draggable = null;
         this.state = null;
-        this.$board = null;
         this.option = null;
-        this.initialized = null;
+        this.hooks = null;
       }
 
       this.removeElements = function() {
@@ -494,6 +495,187 @@
     }).call(Drag.prototype);
 
     return Drag;
+  }()),
+
+  
+  (function DoublyLinkedMapFactory() {
+    'use strict'
+
+
+    function DoublyLinkedMap() {
+      this.head = null;
+      this.tail = null;
+      this.map = Object.create(null);
+      this.length = 0;
+
+      return this.returnObject();
+    }
+
+
+    (function DoublyLinkedMapPrototype() {
+      this.put = function (k, v) {
+        const node = {
+          key: k,
+          value: v,
+          prev: this.tail,
+          next: null,
+        };
+
+        this.map[k] = node;
+
+        if (this.length === 0) {
+          this.head = node;
+        }
+
+        if (this.tail) {
+          this.tail.next = node;
+        }
+
+        this.tail = node;
+        this.length += 1;
+      }
+
+      this.remove = function (k) {
+        const node = this.getNode(k);
+
+        if (!node) {
+          return;
+        }
+
+        if (this.isHead(node.key)) {
+          if (node.next) {
+            node.next.prev = null;
+          }
+          this.head = node.next;
+        }
+
+        if (this.isTail(node.key)) {
+          if (node.prev) {
+            node.prev.next = null;
+          }
+          this.tail = node.prev;
+        }
+
+        if (node.next) {
+          node.next.prev = node.prev;
+        }
+
+        if (node.prev) {
+          node.prev.next = node.next;
+        }
+
+        delete this.map[k];
+        this.length -= 1;
+      }
+
+      this.clear = function () {
+        this.head = null;
+        this.tail = null;
+        this.map = Object.create(null);
+        this.length = 0;
+      }
+      
+      this.destroy = function() {
+        this.head = null;
+        this.tail = null;
+        this.map = null;
+        this.length = null;
+      }
+
+      this.size = function () {
+        return this.length;
+      }
+
+      this.get = function (k) {
+        return this.getNode(k)?.value;
+      }
+
+      this.getNode = function (k) {
+        return Object.prototype.hasOwnProperty.call(this.map, k) ? this.map[k] : null;
+      }
+
+      this.hasNext = function (k) {
+        return this.getNode(k).next != null;
+      }
+
+      this.isHead = function (k) {
+        return this.head === this.getNode(k);
+      }
+
+      this.isTail = function (k) {
+        return this.tail === this.getNode(k);
+      }
+
+      this.getNextKey = function (k) {
+        return this.hasNext(k) ? this.getNode(k).next.key : null;
+      }
+
+      this.contains = function (k) {
+        return Object.prototype.hasOwnProperty.call(this.map, k);
+      }
+
+      this.each = function (callBack = () => false) {
+        const
+          len = this.length;
+
+        let
+          i = 0,
+          param = null,
+          node = this.head;
+
+        while (i < len) {
+          param = {
+            key: node.key,
+            value: node.value,
+          };
+
+          if (callBack(param, i) === false) {
+            break;
+          }
+
+          node = node.next;
+          i++;
+        }
+      }
+
+      this.toArray = function () {
+        const array = [];
+        this.each(({ value }) => array.push(value));
+        return array;
+      }
+
+      this.filter = function (callBack = () => false) {
+        const array = [];
+        this.each((param, i) => {
+          if (callBack(param, i) === true) {
+            array.push(param);
+          }
+        });
+        return array;
+      }
+
+      this.returnObject = function() {
+        return {
+          get: this.get.bind(this),
+          put: this.put.bind(this),
+          remove: this.remove.bind(this),
+          clear: this.clear.bind(this),
+          destroy: this.destroy.bind(this),
+          each: this.each.bind(this),
+          toArray: this.toArray.bind(this),
+          filter: this.filter.bind(this),
+          size: this.size.bind(this),
+          contains: this.contains.bind(this),
+        };
+      }
+
+    }).call(DoublyLinkedMap.prototype);
+
+    return {
+      create() {
+        return new DoublyLinkedMap();
+      },
+    };
   }()),
 
 ))));
